099ea7ea2037967ade122ea80d3c9adc4ef5b894
John Merline <johnmerline@Johns-MacBook-Pro.local>
2019-05-30 10:22:21 -0500

    Clean up main.tf for security component

--- core/security/main.tf
+++ core/security/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "natureofclouds-terraform"
-    key    = "core/network/terraform.tfstate"
+    key    = "core/security/terraform.tfstate"
     region = "us-east-1"
   }
 }
@@ -15,7 +15,4 @@ terraform {
 # external parameter from CodeBuild via TF_VAR_env
 variable "env" {} # Name of new environment/vpc
 
-data "aws_vpcs" "vpcs" {}  # Count the number of existing vpcs to calcuate next free /20 cidr_block
-locals {
-  cidr_block = "${cidrsubnet("172.30.0.0/16", 4, length(data.aws_vpcs.vpcs.ids) - 1)}"
-}
+
